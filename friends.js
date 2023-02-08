c10ba9518cbb8dee9a090bbd2530514b90178c21
Kristin Schlossman <Kristin.Schlossman@gmail.com>
2023-02-08 10:36:26 -0800

    Add friend variables.

--- friends.js
+++ friends.js
@@ -1,6 +1,9 @@
-var friend = ("Susie")
+var friend = "Susie"
+var vgFriend = "Jack"
 if (friend === "Marshall") {
     console.log(`${friend} is my best friend.`)
+} else if (vgFriend === "Jack") {
+    console.log(`${vgFriend} is a very good friend.`)
 } else {
     console.log(`${friend} is a good friend.`)
 }
